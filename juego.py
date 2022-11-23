5d4b9a3869eec6159a4b7f5e5e4f0f19cf7d10ee
SleyAvila <zleysithafc@gmail.com>
2022-11-23 14:00:21 -0500

    Modificacion, falta prueba

--- juego.py
+++ juego.py
@@ -1,8 +1,34 @@
 import random
 
 print("PIEDRA PAPEL O TIJERA")
-opcion = 0
-print("Digite la opcion 0, si desea dejar de jugar")
-while opcion != 0:
+
+punmaq=0
+punuser=0
+intentos =0
+n=int(input("Cuantas veces desea jugar: "))
+
+while  intentos < n:
+    maq = random.randint(1,3)
     print("Seleccione la opcion")
-    n = int(input(""""""))
+    print("""1. PIEDRA
+2. PAPEL
+3. TIJERA""")
+    us= int(input())
+    if us >=1 and us <=3:
+
+        if (maq == 1 and us== 2) or (maq==2 and us== 3) or (maq==3 and us== 1):
+            punuser +=1
+            print("Ganaste")
+        elif (maq == 1 and us == 3) or (maq== 2 and us== 1) or (maq== 3 and us== 2):
+            punmaq +=1
+            print("Perdiste") 
+        elif maq == us:
+            print("Empate")
+            punmaq = punmaq
+            punuser = punuser
+    else:
+        print("Digito una opcion incorrecta")
+    intentos+=1
+
+print(f"puntos Maquina {punmaq}")
+print(f"puntos usuario {punuser}")
