4fec27a14ced1fe9fb7d24c0dd22499835b06157
SleyAvila <zleysithafc@gmail.com>
2022-11-23 13:32:04 -0500

    Creacion del proyecto

---
+++ juego.py
@@ -0,0 +1,8 @@
+import random
+
+print("PIEDRA PAPEL O TIJERA")
+opcion = 0
+print("Digite la opcion 0, si desea dejar de jugar")
+while opcion != 0:
+    print("Seleccione la opcion")
+    n = int(input(""""""))
